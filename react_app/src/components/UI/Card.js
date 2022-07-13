6fb5f0b78c90934a9b7417d17a6a80d407c0e2bc
briansodenkin <briansodenkin@gmail.com>
2022-07-13 22:36:40 +0200

    Finished the dynamic list

--- react_app/src/components/UI/Card.js
+++ react_app/src/components/UI/Card.js
@@ -4,7 +4,6 @@ import './Card.css';
 
 const Card = (props) => {
   const classes = 'card ' + props.className;
-  console.log(classes)
 
   return <div className={classes}>{props.children}</div>;
 };
